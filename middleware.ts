e166f215b456f37d8b42a395f3be132178793a52
ceaseredit <ceaseredit@gmail.com>
2026-04-01 00:47:46 +0100

    s

--- middleware.ts
+++ middleware.ts
@@ -1,4 +1,4 @@
-import { NextResponse } from "next/server";
+import { NextResponse, type NextRequest } from "next/server";
 
 export function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
