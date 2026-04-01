95126cdac2c0c21313dd1fdaff23fdeb7703dcaf
ceaseredit <ceaseredit@gmail.com>
2026-04-01 01:29:16 +0100

    s

--- app/(admin)/admin/dashboard/page.tsx
+++ app/(admin)/admin/dashboard/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+export const dynamic = "force-dynamic";
 import { COLORS } from "@/constants/Theme";
 import Link from "next/link";
 import React, { useEffect, useRef, useState } from "react";
